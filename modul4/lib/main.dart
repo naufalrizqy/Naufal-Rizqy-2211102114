8ba80eb8653a6adb2b8c96b3e70b62bca824d85b
naufalrizqy <rizqynaufal7@gmail.com>
2025-03-24 12:55:04 +0700

    Menambahkan modul 4

---
+++ modul4/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Grid View Example")),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SizedBox(
+            width: 250,
+            height: 250,
+            child: Stack(
+              children: <Widget>[
+                Container(width: 250, height: 250, color: Colors.white),
+                Container(
+                  padding: const EdgeInsets.all(5.0),
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[
+                        Colors.black.withAlpha(0),
+                        Colors.black12,
+                        Colors.black45,
+                      ],
+                    ),
+                  ),
+                  child: const Text(
+                    'Foreground Text',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
